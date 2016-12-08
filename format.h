dfe0a5f34f99addbcd14156610689345fab8c0f1
wherewindblow <wherewindblow@sina.com>
2016-12-09 00:37:27 +0800

    refactor: optimize to_string performance

--- format.h
+++ format.h
@@ -8,6 +8,7 @@
 
 #include <cstddef>
 #include <string>
+#include <limits>
 
 
 namespace lights {
@@ -23,17 +24,87 @@ inline void to_string(std::string& sink, char ch)
 	sink.push_back(ch);
 }
 
-void to_string(std::string& sink, short n);
-void to_string(std::string& sink, unsigned short n);
-void to_string(std::string& sink, int n);
-void to_string(std::string& sink, unsigned int n);
-void to_string(std::string& sink, long n);
-void to_string(std::string& sink, unsigned long n);
-void to_string(std::string& sink, long long n);
-void to_string(std::string& sink, unsigned long long n);
-void to_string(std::string& sink, float n);
-void to_string(std::string& sink, double n);
-void to_string(std::string& sink, long double n);
+inline void to_string(std::string& sink, short n)
+{
+	char buf[std::numeric_limits<short>::digits10 + 1];
+	int writen = std::sprintf(buf, "%hd", n);
+	sink.append(buf, static_cast<std::size_t>(writen));
+}
+
+inline void to_string(std::string& sink, unsigned short n)
+{
+	char buf[std::numeric_limits<unsigned short>::digits10 + 1];
+	int writen = std::sprintf(buf, "%hu", n);
+	sink.append(buf, static_cast<std::size_t>(writen));
+}
+
+inline void to_string(std::string& sink, int n)
+{
+	char buf[std::numeric_limits<int>::digits10 + 1];
+	int writen = std::sprintf(buf, "%d", n);
+	sink.append(buf, static_cast<std::size_t>(writen));
+}
+
+inline void to_string(std::string& sink, unsigned int n)
+{
+	char buf[std::numeric_limits<unsigned int>::digits10 + 1];
+	int writen = std::sprintf(buf, "%u", n);
+	sink.append(buf, static_cast<std::size_t>(writen));
+}
+
+inline void to_string(std::string& sink, long n)
+{
+	char buf[std::numeric_limits<long>::digits10 + 1];
+	int writen = std::sprintf(buf, "%ld", n);
+	sink.append(buf, static_cast<std::size_t>(writen));
+}
+
+inline void to_string(std::string& sink, unsigned long n)
+{
+	char buf[std::numeric_limits<unsigned long>::digits10 + 1];
+	int writen = std::sprintf(buf, "%lu", n);
+	sink.append(buf, static_cast<std::size_t>(writen));
+}
+
+inline void to_string(std::string& sink, long long n)
+{
+	char buf[std::numeric_limits<long long>::digits10 + 1];
+	int writen = std::sprintf(buf, "%lld", n);
+	sink.append(buf, static_cast<std::size_t>(writen));
+}
+
+inline void to_string(std::string& sink, unsigned long long n)
+{
+	char buf[std::numeric_limits<unsigned long long>::digits10 + 1];
+	int writen = std::sprintf(buf, "%llu", n);
+	sink.append(buf, static_cast<std::size_t>(writen));
+}
+
+inline void to_string(std::string& sink, float n)
+{
+	char buf[std::numeric_limits<float>::max_exponent10 + 1 +
+		std::numeric_limits<float>::digits10 + 1];
+	int writen = std::sprintf(buf, "%f", n);
+	sink.append(buf, static_cast<std::size_t>(writen));
+}
+
+inline void to_string(std::string& sink, double n)
+{
+	// 100 is the max exponent10 of double that can format in
+	// sprintf on g++ (GCC) 6.2.1 20160916 (Red Hat 6.2.1-2).
+	char buf[100 + 1 + std::numeric_limits<double>::digits10 + 1];
+	int writen = std::snprintf(buf, sizeof(buf), "%f", n);
+	sink.append(buf, static_cast<std::size_t>(writen));
+}
+
+inline void to_string(std::string& sink, long double n)
+{
+	// 100 is the max exponent10 of long double that can format in
+	// sprintf on g++ (GCC) 6.2.1 20160916 (Red Hat 6.2.1-2).
+	char buf[100 + 1 + std::numeric_limits<long double>::digits10 + 1];
+	int writen = std::snprintf(buf, sizeof(buf), "%Lf", n);
+	sink.append(buf, static_cast<std::size_t>(writen));
+}
 
 
 template <typename T>
