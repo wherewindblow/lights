4545e0546fa8ed1395264a4cecd2e80e33592352
wherewindblow <wherewindblow@sina.com>
2016-12-13 19:23:53 +0800

    feat: add logger that log message to the sink

---
+++ logger.h
@@ -0,0 +1,184 @@
+/**
+ * logger.h
+ * @author wherewindblow
+ * @date   Dec 09, 2016
+ */
+
+#pragma once
+
+#include <cstring>
+#include <ctime>
+
+#include "format.h"
+
+
+namespace lights {
+
+namespace details {
+
+static const char* log_level_names[] = {
+	"debug", "info", "warning", "error", "off"
+};
+
+} // namespace details
+
+
+enum class LogLevel
+{
+	debug = 0, info, warn, error, off
+};
+
+
+inline const char* to_string(LogLevel level)
+{
+	return details::log_level_names[static_cast<int>(level)];
+}
+
+
+/**
+ * Logger message to the backend sink.
+ * @tparam Sink  Support `void write(const char* str, std::size_t len);`
+ */
+template <typename Sink>
+class Logger
+{
+public:
+	Logger(const std::string& name) :
+		m_name(name)
+	{}
+
+	const std::string& get_name() const
+	{
+		return m_name;
+	}
+
+	LogLevel get_level() const
+	{
+		return m_level;
+	}
+
+	void set_level(LogLevel level)
+	{
+		m_level = level;
+	}
+
+
+	template<typename... Args>
+	void log(LogLevel level, const char* fmt, const Args&... args);
+
+	template<typename... Args>
+	void debug(const char* fmt, const Args&... args)
+	{
+		this->log(LogLevel::debug, fmt, args...);
+	}
+
+	template <typename... Args>
+	void info(const char* fmt, const Args&... args)
+	{
+		this->log(LogLevel::info, fmt, args...);
+	}
+
+	template <typename... Args>
+	void warn(const char* fmt, const Args&... args)
+	{
+		this->log(LogLevel::warn, fmt, args...);
+	}
+
+	template <typename... Args>
+	void error(const char* fmt, const Args&... args)
+	{
+		this->log(LogLevel::error, fmt, args...);
+	}
+
+
+	void log(LogLevel level, const char* str)
+	{
+		if (m_level <= level)
+		{
+			std::size_t len = std::strlen(str);
+			m_sink.write(str, len);
+		}
+	}
+
+	void debug(const char* str)
+	{
+		this->log(LogLevel::debug, str);
+	}
+
+	void info(const char* str)
+	{
+		this->log(LogLevel::info, str);
+	}
+
+	void warn(const char* str)
+	{
+		this->log(LogLevel::warn, str);
+	}
+
+	void error(const char* str)
+	{
+		this->log(LogLevel::error, str);
+	}
+
+
+	template <typename T>
+	void log(LogLevel level, const T& value)
+	{
+		if (m_level <= level)
+		{
+			std::string msg = format("{}", value);
+			m_sink.write(msg.c_str(), msg.length());
+		}
+	}
+
+	template <typename T>
+	void debug(const T& value)
+	{
+		this->log(LogLevel::debug, value);
+	}
+
+	template <typename T>
+	void info(const T& value)
+	{
+		this->log(LogLevel::info, value);
+	}
+
+	template <typename T>
+	void warn(const T& value)
+	{
+		this->log(LogLevel::warn, value);
+	}
+
+	template <typename T>
+	void error(const T& value)
+	{
+		this->log(LogLevel::error, value);
+	}
+
+private:
+	std::string m_name;
+	LogLevel m_level = LogLevel::info;
+	Sink m_sink;
+};
+
+
+template <typename Sink>
+template <typename... Args>
+void Logger<Sink>::log(LogLevel level, const char* fmt, const Args&... args)
+{
+	if (m_level <= level)
+	{
+		std::time_t time = std::time(nullptr);
+		std::tm tm;
+		localtime_r(&time, &tm);
+		char buf[50];
+		std::size_t len = std::strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", &tm);
+		buf[len] = '\0';
+		auto str = format("[{}] [{}] [{}]", buf, m_name, to_string(m_level));
+		details::format_impl(str, fmt, args...);
+		str.push_back('\n');
+		m_sink.write(str.c_str(), str.length());
+	}
+}
+
+} // namespace lights
